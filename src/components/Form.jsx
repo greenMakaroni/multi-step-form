3f8ed891d6787de335e8e9be2675cc02fa9414e2
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-17 21:47:36 +0100

    finished the sidebar component, with buttons and text

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,7 +5,7 @@ const Form = ({ isDesktop }) => {
   const [step, setStep] = useState(1)
 
   return (
-    <div className={`${isDesktop ? "bg-[white] w-[60vw] h-[80vh] p-[15px] rounded-xl flex flex-row": "w-screen h-screen"}`}>
+    <div className={`${isDesktop ? "bg-[white] w-[60vw] h-[75vh] m-[0] py-[15px] px-[10px] rounded-xl flex flex-row": "w-screen h-screen"}`}>
         <Sidebar step={step} setStep={setStep} isDesktop={isDesktop} />
     </div>  
   )
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,39 @@
 import Button from "./Button"
 const Sidebar = ({ step, setStep, isDesktop }) => {
 
+    const steps =
+        [{
+            title: "STEP 1",
+            text: "YOUR INFO",
+        },
+        {
+            title: "STEP 1",
+            text: "YOUR INFO",
+        },
+        {
+            title: "STEP 1",
+            text: "YOUR INFO",
+        },
+        {
+            title: "STEP 1",
+            text: "YOUR INFO",
+        },]
+
+
     return (
-        <div className={`flex ${isDesktop ? "bg-sidebar_desktop w-[30%] rounded-xl flex-col" : "w-screen h-1/5 bg-sidebar_mobile flex-row  items-center justify-center"}`}>
-            <Button num={1} step={step} setStep={setStep} />
-            <Button num={2} step={step} setStep={setStep} />
-            <Button num={3} step={step} setStep={setStep} />
-            <Button num={4} step={step} setStep={setStep} />
+        <div className={`flex m-0 ${isDesktop ? " bg-[length:100%_100%] bg-sidebar_desktop w-[30%] rounded-xl flex-col px-[25px] py-[40px]" : "w-screen h-1/5 bg-sidebar_mobile flex-row  items-center justify-center"}`}>
+            {steps.map((s, index) => {
+                return (
+                    <div key={index} className="flex flex-row m-[0] my-[10px]">
+                         
+                         <Button num={index + 1} step={step} setStep={setStep} />
+                         <div className="ml-[10px] flex flex-col justify-center">
+                             <h1 className=""> {s.title} </h1>
+                             <p className="">{s.text}</p>
+                         </div>
+                    </div>
+                    )
+            })}
         </div>
     )
 }
