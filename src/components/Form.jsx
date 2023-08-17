5dc4861039073366a1d313f25de9698627bfdea5
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-17 01:35:21 +0100

    started new challenge, installed tailwind, and created form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+
+const Form = () => {
+  return (
+    <div className="bg-[white] w-[60vw] h-[80vh] p-[15px] rounded-xl flex flex-row">
+        <img src="./bg-sidebar-desktop.svg" alt="side bar background picture" />
+    </div>
+  )
+}
+
+export default Form
