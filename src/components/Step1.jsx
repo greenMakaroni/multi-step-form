e14ee50e7b759b9d670b4fbf2d164fb7d3ba6293
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-21 14:59:54 +0100

    fixed resize bugs, finished styling step_1 component

--- src/components/Step1.jsx
+++ src/components/Step1.jsx
@@ -11,39 +11,41 @@ const Step1 = ({ personalInfo, setPersonalInfo }) => {
     console.log(formJson);
   }
   return (
-    <div className="text-[black] w-[70%] flex flex-col px-[75px] py-[25px]">
+    <div className="text-[black] w-[70%] flex flex-col px-[5vw] py-[2vh] h-[100%]">
       <h1 className="font-['Ubuntu'] text-[#02295a] text-[2rem] font-bold"> Personal info </h1>
-      <p className="mb-[40px] font-['Ubuntu'] text-[16px] font-[300] text-[#9699ab]"> Please provide your name, email address, and phone number.</p>
+      <p className="mb-[5vh] font-['Ubuntu'] text-[16px] font-[300] text-[#9699ab]"> Please provide your name, email address, and phone number.</p>
 
-      <form className="flex flex-col justify-between" onSubmit={handleSubmit}>
-        <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="name">Name</label>
-        <input 
-        name="name" 
-        className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 " 
-        type="text" 
-        defaultValue={personalInfo.name}
-        placeholder="Your name here"
-        /> 
+      <form className="h-[80%] flex flex-col justify-between" onSubmit={handleSubmit}>
 
-        <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="email">Email Address</label>
-        <input 
-        name="email" 
-        className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 " 
-        type="text" 
-        defaultValue={personalInfo.email}
-        placeholder="example@gmail.com"
-        />
+        <div className="flex flex-col">
+          <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="name">Name</label>
+          <input
+            name="name"
+            className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 "
+            type="text"
+            defaultValue={personalInfo.name}
+            placeholder="Your name here"
+          />
 
-        <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="phone">Phone Number</label>
-        <input 
-        name="phone" 
-        className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 " 
-        type="text" 
-        defaultValue={personalInfo.phone}
-        placeholder="e.g. +1 234 567 890"
-        />
+          <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="email">Email Address</label>
+          <input
+            name="email"
+            className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 "
+            type="text"
+            defaultValue={personalInfo.email}
+            placeholder="example@gmail.com"
+          />
 
-        <button className="bg-[#02295a] self-end mt-[14vh] text-[white] font-[500] tracking-wider rounded-lg w-[150px] px-[20px] py-[15px]" type="submit">Next Step</button>
+          <label className="text-[#02295a] mb-[5px] font-['Ubuntu'] font-[300]" htmlFor="phone">Phone Number</label>
+          <input
+            name="phone"
+            className="text-[#02295a] p-[10px] mb-[2vh] rounded-lg bg-white border-2 "
+            type="text"
+            defaultValue={personalInfo.phone}
+            placeholder="e.g. +1 234 567 890"
+          />
+        </div>
+        <button className="bg-[#02295a] self-end text-[white] font-[500] tracking-wider rounded-lg w-[150px] px-[20px] py-[15px]" type="submit">Next Step</button>
       </form>
     </div>
   )
