2525b1b817b78a0474c523f68c5f9a7e67d1e874
greenMakaroni <mrtn.dawid@gmail.com>
2023-08-23 14:07:59 +0100

    bigger margin for plans, add hover effect to plan tiles

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,7 +1,7 @@
 
 const Plan = ({ image, title, price }) => {
   return (
-    <div className="flex flex-col border-2 justify-between w-[30%] rounded-2xl p-[10px]">
+    <div className={`hover:border-[#02295a] flex flex-col border-2 justify-between w-[30%] rounded-2xl p-[10px]`}>
         <img className="w-[45px]" src={image} alt={`${title} plan icon`} />
         <div className="flex flex-col">
             <h1 className="font-['Ubuntu'] text-[#02295a] text-[1rem] font-bold">{title}</h1>
--- src/components/Step2.jsx
+++ src/components/Step2.jsx
@@ -24,7 +24,7 @@ const Step2 = ({ setStep }) => {
       <h1 className="font-['Ubuntu'] text-[#02295a] text-[2rem] font-bold"> Select your plan </h1>
       <p className="mb-[5vh] font-['Ubuntu'] text-[16px] font-[300] text-[#9699ab]"> You have the option of monthly or yearly billing.</p>
       
-      <div className="flex flex-row justify-between w-[100%] h-[35%]">
+      <div className="flex flex-row justify-between w-[100%] h-[35%] mb-[4vh]">
         {plans.map((plan, index) => {
           return(
             <Plan key={index} image={plan.image} title={plan.title} price={plan.price} />
@@ -32,7 +32,7 @@ const Step2 = ({ setStep }) => {
         })}
       </div>
 
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between ">
         <button onClick={() => setStep(1)} className="bg-[white] self-end text-[#02295a] font-[500] tracking-wider rounded-lg w-[150px] px-[20px] py-[15px]" >Go Back</button>
         <button onClick={() => setStep(3)} className="bg-[#02295a] self-end text-[white] font-[500] tracking-wider rounded-lg w-[150px] px-[20px] py-[15px]" >Next Step</button>
       </div>
